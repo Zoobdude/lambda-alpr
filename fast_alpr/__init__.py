f514907b08246faacac305e2ae83f03c9508156e
ankandrew <61120139+ankandrew@users.noreply.github.com>
2026-03-15 19:39:59 -0300

    Export `BoundingBox` in `__all__`

--- fast_alpr/__init__.py
+++ fast_alpr/__init__.py
@@ -3,13 +3,14 @@ FastALPR package.
 """
 
 from fast_alpr.alpr import ALPR, ALPRResult, DrawPredictionsResult
-from fast_alpr.base import BaseDetector, BaseOCR, DetectionResult, OcrResult
+from fast_alpr.base import BaseDetector, BaseOCR, BoundingBox, DetectionResult, OcrResult
 
 __all__ = [
     "ALPR",
     "ALPRResult",
     "BaseDetector",
     "BaseOCR",
+    "BoundingBox",
     "DetectionResult",
     "DrawPredictionsResult",
     "OcrResult",
